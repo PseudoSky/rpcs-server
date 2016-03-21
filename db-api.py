c47b60ee4e7db41ef1d9ba9ad9eac2bda9c03ed0
Aditya Bist <abist@andrew.cmu.edu>
2016-03-21 14:19:35 -0400

    added deleting a user

--- db-api.py
+++ db-api.py
@@ -13,9 +13,9 @@ app = Flask(__name__)
 api = rest.Api(app)
 app.config.from_object(Settings)
 
-
-@app.route('/api/user/<int:user_id>', methods=['GET'])
-def get_user(user_id):    
+#retrieve a user
+@app.route('/api/user/get/<int:user_id>', methods=['GET'])
+def get_user_byID(user_id):    
     try:
         return jsonify({'first': User[user_id].first,
                 'last' : User[user_id].last,
@@ -24,17 +24,21 @@ def get_user(user_id):
     except:
         return "404 Error"
 
-#fix DIS
-@app.route('/api/user/', methods=['POST'])
-def make_user():
-    new_id = db.insert("User", first="aditya", last="bist", phone="12213123123", address="23sdfs", classType="User", 
+#insert a user
+@app.route('/api/user/post/<string:first>/<string:last>/<int:phone>/<string:address>')
+def make_user(first, last, phone, address):
+    new_id = db.insert("User", first=first, last=last, phone=phone, address=address, classType="User", 
         returning='id')
     return jsonify({'new_id' : new_id})
 
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+#delete a user
+@app.route('/api/user/delete/<int:user_id>')
+def delete_user_byID(user_id):
+    try:
+        User[user_id].delete()
+        return "User deleted"
+    except:
+        return "404 Error"
 
 if __name__ == '__main__':
     app.wsgi_app = orm.db_session(app.wsgi_app)
